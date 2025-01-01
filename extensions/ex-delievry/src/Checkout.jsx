c3eb9783b1e21d9c1c0506b2ec55da885a19de1c
Muhammad Zahid <119036431+zahid-afridi@users.noreply.github.com>
2025-01-01 16:39:05 +0500

    latest ez........

--- extensions/ex-delievry/src/Checkout.jsx
+++ extensions/ex-delievry/src/Checkout.jsx
@@ -6,10 +6,11 @@ import {
   TextField,
   Text,
   Checkbox,
-  useApi,
+useBuyerJourney,
   useApplyAttributeChange,
   useInstructions,
   useTranslate,
+
   Banner,
   Image
 } from "@shopify/ui-extensions-react/checkout";
@@ -25,12 +26,12 @@ export default reactExtension("purchase.checkout.block.render", () => (
 function Extension() {
  
   const translate = useTranslate();
-  const { extension } = useApi();
+  
   const {amount}=useTotalAmount();
   const instructions = useInstructions();
   const applyAttributeChange = useApplyAttributeChange();
- 
- console.log('checkout',amount)
+  
+  console.log('checkout',amount)
   const [isEZDeliverySelected, setIsEZDeliverySelected] = React.useState(true);
   const [checked, setCheck] = React.useState({
     sameday: false,
@@ -39,7 +40,69 @@ function Extension() {
   });
   const [selectedDeliveryOption, setSelectedDeliveryOption] = React.useState("");
   const [selectedTimeSlot, setSelectedTimeSlot] = React.useState("");
+  
+  const buyerJourney = useBuyerJourney();
 
+  // Log the buyerJourney result to inspect the data
+  React.useEffect(() => {
+    console.log("Buyer Journey:", buyerJourney);
+
+    // Check if the buyer has completed the order and log a success message
+    const unsubscribeCompleted = buyerJourney.completed.subscribe(async(completed) => {
+      if (completed) {
+      console.log('api hiting')
+      const data={
+        "customerId": "ckgpMY0eIwXcsSoJJx09h",
+        "pickup": {
+            "address": "KARACHI",
+            "coordinates": [-0.15869881563038823, 51.51268479847148],
+            "fullName": "ALI ATHER",
+            "phone": "611522",
+            "email": "abcd@pickup.org"
+        },
+        "delivery": {
+            "address": "KARACHI ",
+            "addressDetail": "block A2",
+            "coordinates": [-0.1405885408344662, 51.50785039521855],
+            "fullName": "mosi pann",
+            "phone": "1154123",
+            "email": "abcd@delivery.org"
+        },
+        "service": {
+            "id": "1Vr4gyMBELnYNyXIpyYHE",
+            "options": [
+                { "id": "1Vr4gyMBELnYNyXIpyYHE", "dataId": "QQNWe9WRid2y0mOA0bmt_" },
+                { "id": "JlI_Ez5wyNuXMHq4FCm_m", "inputValue": "20 lbs" },
+                { "id": "ElGWocOJaKXTcjW782jbr", "dataId": "" }
+            ]
+        },
+        "paymentMethod": "Cash",
+        "paymentSide": "Sender",
+        "draft": true,
+        "note": "note",
+        "referenceId": "ref#112"
+    }
+     try {
+      const req=await fetch('https://wrmx.manage.onro.app/api/v1/customer/order/pickup-delivery', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNrZ3BNWTBlSXdYY3NTb0pKeDA5aCIsInNvdXJjZSI6ImJ1c2luZXNzIiwiaWF0IjoxNzM1NTYyODExLCJleHAiOjE3MzU1NjY0MTF9.FnYkTE91Qt45xtJNAtqtp2yfEW2EY5pflnc8AzA85hM'
+        },
+        body: JSON.stringify(data),
+      });
+      const response=await req.json();
+      console.log('order deivlerd',response)
+     } catch (error) {
+      console.log('order deiliver api erro',error)
+     }
+        console.log("Order placed successfully");
+      }
+    });
+
+    // Clean up the subscription when the component unmounts
+    return () => unsubscribeCompleted();
+  }, [buyerJourney]);
   const DeliveryCheck = (name) => {
     if (name === "sameday") {
       setSelectedDeliveryOption(name);
